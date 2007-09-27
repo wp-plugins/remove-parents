821ac52507ea9cdc891a9303332152e8b9fec830
alekc <alekc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-09-27 09:47:38 +0000

    Prima Versione
    git-svn-id: https://plugins.svn.wordpress.org/remove-parents/trunk@20167 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Plugin Name ===
+Contributors: 
+Donate link: 
+Tags: parent, category, child, permanent
+Requires at least: 2.3
+Tested up to: 2.3
+Stable tag: 4.3
+
+Remove Parents is a tiny plugin which remove "category" and parent categories from permalink.
+
+== Description ==
+
+Sometimes you don't want to show parent directories in permalink because they are not really necessary or because you know that in the future your child directory will change it's parent.
+
+For example:
++Work in progress
+--Site 1
+--Site 2
++Work Finished
+--Site 3
+
+In this case Site 3 originally have been part of "Work in progress", and on job done it has been moved under "Work Finished".
+
+So if you install this plugin your link will be transformed in this way:
+
+http://domain.com/parent_category/child_directory/post_name => http://domain.com/child_directory/post_name
+http://domain.com/category/parent_category => http://domain.com/parent_category
+http://domain.com/category/parent_category/child_directory => http://domain.com/parent_category/child_directory
+
+== Installation ==
+
+1. Upload `remove_parents` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Set your permalink structure as "/%category%/%postname%" (or something like "/%category%/%postname%.extension")
+4. Set your Category Base empty.
+5. Test everything and let me know if you find some bug ;)
---
+++ remove-parent.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Plugin Name: Kill Parents
+Plugin URI: http://wordpress.org/#
+Description: Kills parent directories from showing in permalinks.
+Author: Alekc
+Version: 1.0
+Author URI: http://blog.alekc.org
+*/
+/*  Copyright 2007  Alexander Chernov  (email : alekcander@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+function kill_parent_cats($namedcatpath) {
+	$result = $namedcatpath;
+	$bloghome = get_bloginfo( 'home' );
+	
+	//removing "category" & parent cats
+	//domain/category/parent_category/subcat
+	if (preg_match('%' . $bloghome . '/category/.*/(.*?)$%i', $namedcatpath))
+		$result =  preg_replace('%' . $bloghome . '/category/.*/(.*?)$%i', $bloghome . '/$1', $namedcatpath);
+	//remove "category" in domain/category/parent_dir.
+	else if (preg_match('%' . $bloghome . '/category/(.*?)$%i', $namedcatpath))
+		$result = preg_replace('%' . $bloghome . '/category/(.*?)$%i', $bloghome . '/$1', $namedcatpath);
+	return $result;	
+}
+function kill_parent_postlink($link) {
+	$result = $link;
+	//domain/parent_cat/sub_cat/post_name
+	$bloghome = get_bloginfo( 'home' );
+	if (preg_match('%' . $bloghome . '/.*?/(.*?)/(.*?)$%i', $link))
+		$result = preg_replace('%' . $bloghome . '/.*?/(.*?)/(.*?)$%i', $bloghome . '/$1/$2', $link);
+		
+	return $result;
+}
+add_filter('category_link', 'kill_parent_cats');
+add_filter('post_link','kill_parent_postlink');
+?>
